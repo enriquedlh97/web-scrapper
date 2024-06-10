08c538f6b2c938d4d19ecf39311d651c0fdaf27e
enriquedlh97 <enriquedlh97@hotmail.com>
2024-06-10 14:43:30 -0600

    refactor: add missing type annotations

--- web_scrapper/scrappers/audi/utils.py
+++ web_scrapper/scrappers/audi/utils.py
@@ -2,12 +2,13 @@ import logging
 
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 
 
 def close_cookie_banner(driver: WebDriver) -> None:
     try:
         logging.info("Attemping to close cookies banner if there is one")
-        close_button = driver.find_element(
+        close_button: WebElement = driver.find_element(
             By.CSS_SELECTOR,
             "button.ca-button.ca-secondary-button.ca-secondary-button-first.go241155495.ca-button-opt-in",  # noqa: E501
         )
